a8163f8dd05ee78e7dd5a37eb3d74e21d51c7c5b
Jan Chow <tobelhead@gmail.com>
2022-10-19 11:32:43 -0600

    added connection to sql and db cmd retrieval

---
+++ main.py
@@ -0,0 +1,48 @@
+import sys
+import sqlite3
+
+connection = None
+cur = None
+
+def connect(path):
+    # connect to sqlite
+
+    global connection, cur
+    connection = sqlite3.connect(path)
+    cur = connection.cursor()
+    cur.execute("PRAGMA foreign_keys = ON;")
+    connection.commit()
+    return
+
+def login():
+    # login screen to retrieve account and detect if account is user, artist, or both
+
+    account_id = input("Please enter your id: ")
+    password = input("Please enter your password: ")
+
+    detect_user_query = """
+                        """
+
+    detect_artist_query = """
+                        """
+
+def main():
+    global connection, cur
+
+    # retrieve database from command line
+    db = sys.argv[1]
+
+    # retrieve path to connect to
+    path = "./{database}".format(database=db)
+    connect(path) 
+
+    # main program
+    login()
+    
+    # close connection and finish program
+    connection.commit()
+    connection.close()
+    return
+
+if __name__ == "__main__":
+    main()
